c6a921b9105a4c6f2bbee31f887ed031f7a6e2bf
Gdz.Cp-Pc <eryprasetyo.15@gmail.com>
2021-08-11 19:16:41 +0700

    Feature Task done

---
+++ Assets/Scirpt/ObjectTrigger.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class ObjectTrigger : MonoBehaviour
+{
+    private Animation anim;
+
+    public void Awake()
+    {
+        anim = GetComponent<Animation>();
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        anim.Play("Play");
+    }
+}
---
+++ Assets/Scirpt/Task_Content.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Task_Content : MonoBehaviour
+{
+
+    [SerializeField] private Text[] tasks;
+    public string[] taskTexts { get; private set; }
+
+    private void Start()
+    {
+        ArrayTugas();
+        SetText();
+    }
+
+    private void SetText()
+    {
+        for(int i = 0; i < tasks.Length; i++)
+        {
+            tasks[i].text = taskTexts[i];
+        }
+    }
+
+    private void ArrayTugas()
+    {
+        Scene scene = SceneManager.GetActiveScene();
+
+        if(scene == SceneManager.GetSceneByName("Gameplay_3"))
+        {
+            taskTexts = new string[3];
+            taskTexts[0] = "Mencoba sebuah text yang \n sangat panjang bener adalah ada disini ";
+            taskTexts[1] = "Mencoba sebuah text yang \n sangat panjang bener adalah ada disini ";
+            taskTexts[2] = "Mencoba sebuah text yang \n sangat panjang bener adalah ada disini ";
+        }
+
+    }
+
+}
